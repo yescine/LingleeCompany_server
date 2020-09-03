72d96706330ea44ca5d619c37c050acd78d034c0
yescine <bouchoucha.yassine@gmail.com>
2020-09-03 18:11:05 +0100

    change mail header

--- src/templates/mail/draftmail.js
+++ src/templates/mail/draftmail.js
@@ -11,15 +11,15 @@ export const draftMail = (data)=>{
          
       case 'restore-password':
          return(`<div>
-               <h1>Link for restore password </h1>
+               <h1>Link to restore password </h1>
                <p>Your new password: ${data.password}</p>
                <p>click on the link to confirm the ownership of your password restoration: <span><a href="http://${data.link}/">yes restore my password</a> or ignore this email</span> </p>
             </div>`
          ) 
       case 'confirm-restore-password':
          return(`<div>
-               <h1>Link for confirm restore password </h1>
-               <p>password restored successfully</p>
+               <h1>password restored successfully </h1>
+               <p>you can sign in to your account</p>
             </div>`
          )      
    
